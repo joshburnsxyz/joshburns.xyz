5f5476e27ea1c4b3dfdb2876488ef7781f43826f
Josh Burns <joshyburnss@gmail.com>
2022-06-29 16:16:12 +1000

    update default config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `JOSHBURNS.XYZ`,
+    description: `My personal webpage.`,
+    author: `@joshburnsxyz`,
+    siteUrl: `https://joshburns.xyz/`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,

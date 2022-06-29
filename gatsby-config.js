303348a9f053df3bd97471b8cb5211fc1db95f12
Josh Burns <joshyburnss@gmail.com>
2022-06-30 00:34:30 +1000

    get blog posts list

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,6 +15,14 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `posts`,
+        path: `${__dirname}/src/posts`,
+      }
+    },
+    `gatsby-plugin-mdx`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -4,25 +4,57 @@ import { StaticImage } from "gatsby-plugin-image"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import * as styles from "../components/index.module.css"
+import { useStaticQuery, graphql } from "gatsby"
 
-const BlogPage = () => (
-  <Layout>
-    <Seo title="Blog" />
-    <div className={styles.textCenter}>
-      <StaticImage
-        src="../images/example.png"
-        loading="eager"
-        width={64}
-        quality={95}
-        formats={["auto", "webp", "avif"]}
-        alt=""
-        style={{ marginBottom: `var(--space-3)` }}
-      />
-      <h1>
-        My Blog
-      </h1>
-    </div>
-  </Layout>
-)
+const BlogPage = () => {
+const data = useStaticQuery(graphql`
+query {
+  allMdx {
+    edges {
+      node {
+        id
+        slug
+        frontmatter {
+          title
+        }
+      }
+    }
+  }
+}
+`)
+
+  return (
+    <Layout>
+      <Seo title="Blog" />
+      <div className={styles.textCenter}>
+        <StaticImage
+          src="../images/example.png"
+          loading="eager"
+          width={64}
+          quality={95}
+          formats={["auto", "webp", "avif"]}
+          alt=""
+          style={{ marginBottom: `var(--space-3)` }}
+        />
+        <h1>
+          My Blog
+          <br/>
+          <br/>
+          <hr/>
+          <div className={styles.textCenter}>
+            {
+              data.allMdx.edges.map(e => (
+                <span>
+                  <p><a href="#">{e.node.frontmatter.title}</a></p>
+                  <small>lorem</small>
+                </span>
+              ))
+            }
+          </div>
+        </h1>
+      </div>
+    </Layout>
+  )
+}
 
 export default BlogPage

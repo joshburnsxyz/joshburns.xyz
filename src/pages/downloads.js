3990eb05b176f529b19e33fd9a350db27baf4747
Josh Burns <83684810+joshburnsxyz@users.noreply.github.com>
2022-06-30 10:37:39 +1000

    Create downloads.js

---
+++ src/pages/downloads.js
@@ -0,0 +1,39 @@
+import * as React from "react"
+import { StaticImage } from "gatsby-plugin-image"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+import * as styles from "../components/index.module.css"
+import { useStaticQuery, graphql } from "gatsby"
+
+const DownloadsPage = () => {
+  return (
+    <Layout>
+      <Seo title="Blog" />
+      <div className={styles.textCenter}>
+        <StaticImage
+          src="../images/example.png"
+          loading="eager"
+          width={64}
+          quality={95}
+          formats={["auto", "webp", "avif"]}
+          alt=""
+          style={{ marginBottom: `var(--space-3)` }}
+        />
+        <h1>
+          Downloads
+          <br/>
+          <br/>
+          <hr/>
+          <div>
+            <ul>
+              <li><a href="#"><pre><code>laptop.sh</code></pre> script to provision new machines (MacOS)</a></li>
+            </ul>
+          </div>
+        </h1>
+      </div>
+    </Layout>
+  )
+}
+
+export default DownloadsPage
